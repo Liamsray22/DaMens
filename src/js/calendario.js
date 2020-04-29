a01ecf85ccdc24f40e75d6ebe745d95d52f22593
Jesus Alberto Mercado Jimenez <anotherjez2@gmail.com>
2020-04-29 18:08:25 -0400

    calendario fix

--- src/js/calendario.js
+++ src/js/calendario.js
@@ -2,10 +2,10 @@ var events = [
   {'Date': new Date(2020, 0, 28), 'Title': 'Esto funciona!'},
   {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
   {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
-];
+]; //Eventos de ejemplo
 
-// let json = require('./test.json')
-var json = $.getJSON("test.json", function(json) {
+
+$.getJSON("test.json", function(json) {
   console.log(json); // this will show the info it in firebug console
   array.forEach(json => {
     events.push({'Date': new Date(json.fecha), 'Title': 'Hice mis ejercicios!'})
