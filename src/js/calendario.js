00978ce0fd4656f1656b4934d27c1985d9c1e6d7
Jesus Alberto Mercado Jimenez <anotherjez2@gmail.com>
2020-04-29 17:24:54 -0400

    calendario

---
+++ src/js/calendario.js
@@ -0,0 +1,14 @@
+// function GetEvents(){
+
+//   return []
+// }
+
+var events = [
+  {'Date': new Date(2020, 0, 28), 'Title': 'Esto funciona!'},
+  {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
+  {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+];
+
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
