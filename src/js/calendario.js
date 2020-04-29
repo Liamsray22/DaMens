1b627908c99996e2b5a5dbd8dfce5fd86eef6130
Jesus Alberto Mercado Jimenez <anotherjez2@gmail.com>
2020-04-29 19:07:47 -0400

    calendario fix

--- src/js/calendario.js
+++ src/js/calendario.js
@@ -5,13 +5,21 @@ var events = [
 ]; //Eventos de ejemplo
 
 
-var json = $.getJSON("test.json", function(json) {
-  console.log(json); // this will show the info it in firebug console
-  array.forEach(json => {
-    console.log(Date.parse(json.fecha))
-    events.push({'Date': new Date(Date.parse(json.fecha)), 'Title': 'Hice mis ejercicios!'})
+// var json = $.getJSON("test.json", function(json) {
+//   console.log(json); // this will show the info it in firebug console
+//   array.forEach(json => {
+//     console.log(Date.parse(json.fecha))
+//     events.push({'Date': new Date(Date.parse(json.fecha)), 'Title': 'Hice mis ejercicios!'})
+//   });
+// });
+
+function read_json() {
+  $.getJSON("/test.json", function(data) {
+      $.each(data["events"], function(idx,events) {
+          alert("Prime number: " + events.fecha);
+      });
   });
-});
+}
 
 var settings = {};
 var element = document.getElementById('caleandar');
